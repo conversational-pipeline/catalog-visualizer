0e64e8c6178e991ae46450e2402f19e68376985c
KSLHacks <leung.kevin23@gmail.com>
2019-04-01 16:06:09 -0600

    updating search output view for disambiguation concepts or identified item

--- src/components/TransformItem.vue
+++ src/components/TransformItem.vue
@@ -1,44 +1,42 @@
 <template>
-    <div id="find-item">
-        <form action="" method="get" v-on:submit.prevent="onSubmit">
-            <input type="text" name="labels" id="find-labels">
-            <button type="submit">Get Item</button>
-        </form>
-        <!-- <div>{{ jsonValue }}</div> -->
-        <vue-json-pretty id="json-viewer"
-            :data="jsonValue">
-            </vue-json-pretty>
-    </div>
+  <div id="find-item">
+    <form action method="get" v-on:submit.prevent="onSubmit">
+      <input type="text" name="labels" id="find-labels">
+      <button type="submit">Get Item</button>
+    </form>
+    <!-- <div>{{ jsonValue }}</div> -->
+    <vue-json-pretty id="json-viewer" :data="jsonValue"></vue-json-pretty>
+  </div>
 </template>
 
 <script lang="ts">
-const VueJsonPretty  = require('vue-json-pretty')
+const VueJsonPretty = require("vue-json-pretty");
 
 export default {
-    name: 'FindItem',
-    components: {
-        'vue-json-pretty': VueJsonPretty.default
-    },
-    data () {
-        return {
-            jsonValue: null
-        }
-    },
-    methods: {
-        onSubmit(evt: any) {
-            const labels = evt.target.labels.value
-            return fetch(`http://localhost:3000/search?labels=${labels}`)
-                .then(res => res.json())
-                .then(data => {
-                    this.jsonValue = data
-                })
-        }
+  name: "FindItem",
+  components: {
+    "vue-json-pretty": VueJsonPretty.default
+  },
+  data() {
+    return {
+      jsonValue: null
+    };
+  },
+  methods: {
+    onSubmit(evt: any) {
+      const labels = evt.target.labels.value;
+      return fetch(`http://localhost:3000/search?labels=${labels}`)
+        .then(res => res.json())
+        .then(data => {
+          this.jsonValue = data;
+        });
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 #json-viewer {
-    text-align: left;
+  text-align: left;
 }
 </style>
